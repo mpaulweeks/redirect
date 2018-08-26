39689a083b3b61e606c878923cf90b8c863a3fe4
mpaulweeks <mpaulweeks@gmail.com>
2018-08-26 00:03:42 -0400

    successfully load links

--- packages/api/src/app.js
+++ packages/api/src/app.js
@@ -1,10 +1,35 @@
 'use strict'
 
-const express = require('express')
-const app = express()
+const express = require('express');
+const AWS = require('aws-sdk');
+
+const app = express();
+const s3 = new AWS.S3();
+
+function getLinks(res) {
+  // 'https://s3.amazonaws.com/mpaulweeks-redirect/links.json'
+  return new Promise((resolve, reject) => {
+    s3.getObject({
+      Bucket: 'mpaulweeks-redirect',
+      Key: 'links.json',
+    }, (error, data) => {
+      if (error != null) {
+        console.log('Failed to retrieve an object: ' + error);
+        reject(error);
+      } else {
+        const links = JSON.parse(data.Body.toString());
+        resolve(links);
+      }
+    });
+  }).catch(error => {
+    res.send('error: ' + error);
+  });
+}
 
 app.get('/', (req, res) => {
-  res.send('api hello world')
+  getLinks().then(links => {
+    res.send('api hello world: ' + JSON.stringify(links))
+  });
 })
 
 module.exports = app
