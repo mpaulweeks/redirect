436da1e9e7524594b754998d1e7e1a56497efa0d
mpaulweeks <mpaulweeks@gmail.com>
2018-08-25 23:08:47 -0400

    shell react app

---
+++ packages/fe/src/Admin.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  text-align: center;
+  max-width: 700px;
+  margin: 10px auto;
+`;
+
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        admin view
+      </Container>
+    );
+  }
+}
+
+export default App;
